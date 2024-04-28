91314914c3abfa55fb80a1545ae8c3f2746960a1
CarlosJaramilloTinajero <75448585+CarlosJaramilloTinajero@users.noreply.github.com>
2024-04-27 19:49:33 -0600

    Agregue el user_id a la tabla de tokens

--- app/Models/TokenUser.php
+++ app/Models/TokenUser.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TokenUser extends Model
 {
     use HasFactory;
     protected $table = 'tokens_with_expiraions';
-    protected $fillable = ['token', 'expired_at', 'expired'];
+    protected $fillable = ['token', 'expired_at', 'expired', 'user_id'];
+
+    function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
